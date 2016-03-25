9be9e64b6f1daed4c466057b9675660bb160665f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-03-25 11:15:01 +0100

    Generate song slugs

--- backend/static-api.js
+++ backend/static-api.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const ditty = require('parse-ditty');
 const pick = require('1-liners/pick');
+const slugify = require('speakingurl');
 
 const dataPath = `${__dirname}/../data`;
 const rawCategories = fs.readdirSync(dataPath);
@@ -30,9 +31,13 @@ const songsData = categoryDirs.reduce((result, category) => {
   const songFiles = fs.readdirSync(categoryPath);
   const parsedSongs = songFiles.map(filename => {
     const rawSong = fs.readFileSync(`${categoryPath}/${filename}`, 'utf8');
+    const song = ditty(rawSong);
     return Object.assign({},
-      pick(['number', 'title', 'blocks'], ditty(rawSong)),
-      { category: category.id }
+      pick(['number', 'title', 'blocks'], song),
+      {
+        category: category.id,
+        slug: slugify(song.title),
+      }
     );
   });
   return result.concat(parsedSongs);
