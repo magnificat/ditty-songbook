8e7d60432831a474ce85e81a01e679f6cb9fe0e5
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-03-26 12:40:13 +0100

    Slim down (and break 🙁) the router

--- frontend/BabyLibs/SimpleRouter.elm
+++ frontend/BabyLibs/SimpleRouter.elm
@@ -1,39 +1,26 @@
-module TransitRouter
-  ( WithRoute, TransitRouter, Action, Config
+-- Adapted from etaque/elm-transit-router
+
+module BabyLibs.SimpleRouter
+  ( WithRoute, SimpleRouter, Action, Config
   , actions, empty, init, update
-  , getRoute, getTransition, pushPathAddress
+  , getRoute, pushPathAddress
   ) where
-
-{-|
-Drop-in router with transitions for animated, single page apps. See README for usage.
-
-# Types
-@docs WithRoute, TransitRouter, Action, Config
-
-# Actions
-@docs actions, empty, init, update
-
-# Views
-@docs getRoute, getTransition, pushPathAddress
--}
-
 
 import History
 import Effects exposing (Effects, Never, none)
 import Task exposing (Task)
-import Signal exposing (..)
+import Signal
 
-import Transit
-import Response exposing (..)
+import Response exposing (Response, res, mapEffects, taskRes, withNone)
 
 
 {-| Type extension for the model. -}
-type alias WithRoute route model = { model | transitRouter : TransitRouter route }
+type alias WithRoute route model = { model | transitRouter : SimpleRouter route }
 
 {-| State of the router. -}
-type TransitRouter route = TR (State route)
+type SimpleRouter route = SR (State route)
 
-type alias State route = Transit.WithTransition
+type alias State route =
   { route : route
   , path : String
   }
@@ -44,7 +31,6 @@ type Action route =
   | PushPath String
   | PathUpdated String
   | SetRoute route
-  | TransitAction (Transit.Action (Action route))
 
 
 {-| Signal for path updates, feed your app with this as input. -}
@@ -57,7 +43,7 @@ actions =
 
 
 {-| Private. -}
-mailbox : Mailbox (Action route)
+mailbox : Signal.Mailbox (Action route)
 mailbox =
   Signal.mailbox NoOp
 
@@ -71,36 +57,34 @@ mailbox =
           "click"
           { stopPropagation = True, preventDefault = True }
           Json.value
-          (\_ -> message TransitRouter.pushPathAddress path)
+          (\_ -> message SimpleRouter.pushPathAddress path)
       ]
  -}
-pushPathAddress : Address String
+pushPathAddress : Signal.Address String
 pushPathAddress =
   Signal.forwardTo mailbox.address PushPath
 
 
 {-| Config record for router behaviour:
  * `mountRoute`: what should be the result of a route update (previous route, new route, model) on your model & effects
- * `getDurations`: durations for `Enter` (before `routeUpdate`) and `Exit` (after) phases, same params as `mountRoute`
  * `actionWrapper`: wrapper for router actions into your own action type, to be consistent with `mountRoute` result
  * `routeDecoder`: to transform a path to a route (see `etaque/elm-route-decoder`)
  -}
 type alias Config route action model =
   { mountRoute : route -> route -> (WithRoute route model) -> Response (WithRoute route model) action
-  , getDurations : route -> route -> (WithRoute route model) -> (Float, Float)
   , actionWrapper : Action route -> action
   , routeDecoder : String -> route
   }
 
 
 {-| Empty state for model initialisation (route should render nothing, like EmptyRoute). -}
-empty : route -> TransitRouter route
+empty : route -> SimpleRouter route
 empty route =
-  TR { route = route, path = "", transition = Transit.initial }
+  SR { route = route, path = "" }
 
 
 {-| Start the router with this config and an initial path. Returns host's model and action. -}
-init : Config route action model -> String -> WithRoute route model -> Response (WithRoute route model) action
+-- init : Config route action model -> String -> WithRoute route model -> Response (WithRoute route model) action
 init config path model =
   update config (SetRoute (config.routeDecoder path)) model
 
@@ -109,13 +93,13 @@ init config path model =
 getState : WithRoute route model -> State route
 getState model =
   case model.transitRouter of
-    TR state -> state
+    SR state -> state
 
 
 -- Private: set state in model
 setState : WithRoute route model -> State route -> WithRoute route model
 setState model state =
-  { model | transitRouter = TR state }
+  { model | transitRouter = SR state }
 
 
 -- Private: update state in model
@@ -125,10 +109,9 @@ updateState f model =
 
 
 {-| Update the router with this config, for a given action. Returns host's model and action. -}
-update : Config route action model -> Action route -> WithRoute route model -> Response (WithRoute route model) action
+-- update : Config route action model -> Action route -> WithRoute route model -> Response (WithRoute route model) action
 update config action model =
   case action of
-
     PushPath path ->
       let
         newModel = updateState (\s -> { s | path = path }) model
@@ -142,11 +125,9 @@ update config action model =
       let
         state = getState model
         newRoute = config.routeDecoder path
-        (exit, enter) = config.getDurations state.route newRoute model
-        timeline = Transit.timeline exit (SetRoute newRoute) enter
       in
-        Transit.init TransitAction timeline (getState model)
-          |> mapBoth (setState model) config.actionWrapper
+        setState model newRoute
+          |> withNone
 
     SetRoute route ->
       let
@@ -156,10 +137,6 @@ update config action model =
       in
         config.mountRoute prevRoute route newModel
 
-    TransitAction transitAction ->
-      Transit.update TransitAction transitAction (getState model)
-        |> mapBoth (setState model) config.actionWrapper
-
     NoOp ->
       res model none
 
@@ -168,11 +145,3 @@ update config action model =
 getRoute : WithRoute route model -> route
 getRoute model =
   getState model |> .route
-
-
-{-| Get current transition from model.
-See etaque/elm-transit for transition handling.
--}
-getTransition : WithRoute route model -> Transit.Transition
-getTransition model =
-  getState model |> .transition
