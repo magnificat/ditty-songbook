3e1bb1f9762a36387d871b6cdb86048c4b73c48f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-04-13 21:47:40 +0200

    Prepare SPA setup

--- frontend/styles/fonts.style.js
+++ frontend/styles/fonts.style.js
@@ -1,12 +1,12 @@
 module.exports = [{
   '@font-face': {
     'font-family': '"Merriweather Light"',
-    'src': 'url("fonts/merriweather-light.woff") format("woff")',
+    'src': 'url("/fonts/merriweather-light.woff") format("woff")',
   },
 }, {
   '@font-face': {
     'font-family': '"Merriweather Light"',
     'font-style': 'italic',
-    'src': 'url("fonts/merriweather-light-italic.woff") format("woff")',
+    'src': 'url("/fonts/merriweather-light-italic.woff") format("woff")',
   },
 }];
