c9ae6ac6f293df40e23619f7a41094db3fc90b04
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-04-16 20:25:12 +0200

    Offline index.html

--- static/worker.js
+++ static/worker.js
@@ -1,93 +1,120 @@
-const cacheName = 'v1/shell';
+// Settings
+const cacheName =
+  'v1/shell';
 
-const alwaysFetchThese = [
-  '/',
+const indexHtmlLocation =
+  '/index.html';
+
+const appShellResources = [
   '/scripts.js',
   '/fonts/merriweather-light.woff',
   '/fonts/merriweather-light-italic.woff',
 ];
 
-const preferCache = new RegExp([
-  '\.html$',
-  '\.ico$',
-  '\.css$',
-  '^/scripts\.js$',
-].join('|'));
-
-const preferNetwork = new RegExp([
-  '^/api/.*\.json$',
-].join('|'));
+const fetchStrategies = [{
+  name: 'get index.html',
+  pattern:
+    /^(?:\/[^.]*)?$/,
+}, {
+  name: 'prefer cache',
+  pattern: new RegExp([
+    '\.html$',
+    '\.ico$',
+    '\.css$',
+    '^/scripts\.js$',
+  ].join('|')),
+}, {
+  name: 'prefer network',
+  pattern: new RegExp([
+    '^/api/.*\.json$',
+  ].join('|')),
+}];
 
 
 // Pre-fetch app shell.
+const indexHtmlRequest =
+  new Request(indexHtmlLocation);
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(cacheName)
-      .then((cache) => (
-        cache.addAll(alwaysFetchThese)
-      ))
+      .then((cache) => {
+        cache.add(indexHtmlRequest);
+        cache.addAll(appShellResources);
+      })
   );
 });
 
 
 // Handle requests.
+const fetchFromCache = (request) => () => (
+  caches.match(request).then((response) => {
+    if (response === undefined) {
+      throw new Error(`Nothing found in the cache for ${request.url}`);
+    }
+
+    return response;
+  })
+);
+
+const putInCache = (request) => (response) => {
+  if (response.ok) {
+    const responseCopy = response.clone();
+    caches.open(cacheName)
+      .then(cache => cache.put(request, responseCopy));
+  }
+
+  return response;
+};
+
+let looksLikeWereOffline =
+  false;
+
+const rememberToUpdateCache = (request) => (cachedResponse) => {
+  // Schedule cache update
+  if (!looksLikeWereOffline) {
+    fetch(request)
+      .then(putInCache(request))
+      .catch(() => { looksLikeWereOffline = true; });
+  }
+
+  return cachedResponse;
+};
+
+const fetchNetworkFirst = (request) => () => (
+  fetch(request)
+    .then(putInCache(request))
+    .catch(fetchFromCache(request))
+);
+
+const respondCacheFirst = (event, request) => {
+  event.respondWith(
+    fetchFromCache(request)()
+      .then(rememberToUpdateCache(request))
+      .catch(fetchNetworkFirst(request))
+  );
+};
+
 self.addEventListener('fetch', (event) => {
   const request = event.request;
   const url = new URL(request.url);
-  const fetchStrategy = (
-    (preferCache.test(url.pathname) && 'cache first') ||
-    (preferNetwork.test(url.pathname) && 'network first') ||
-    null
-  );
+  const fetchStrategy = fetchStrategies.find(strategy => (
+    strategy.pattern.test(url.pathname)
+  ));
 
   const criteria = [
     request.method === 'GET',
     url.origin === self.location.origin,
-    fetchStrategy !== null,
+    fetchStrategy !== undefined,
   ];
 
   if (!criteria.every(passed => passed)) return;
 
-  const putInCache = (response) => {
-    if (response.ok) {
-      const responseCopy = response.clone();
-      caches.open(cacheName)
-        .then(cache => cache.put(request, responseCopy));
-    }
-
-    return response;
-  };
-
-  const fetchFromCache = () => (
-    caches.match(request).then((response) => {
-      if (response === undefined) {
-        throw new Error(`Nothing found in the cache for ${request.url}`);
-      }
-
-      return response;
-    })
-  );
-
-  const fetchNetworkFirst = () => (
-    fetch(request)
-      .then(putInCache)
-      .catch(fetchFromCache)
-  );
-
-  const rememberToUpdateCache = (cachedResponse) => {
-    // Schedule cache update
-    fetch(request).then(putInCache);
-
-    return cachedResponse;
-  };
-
-  if (fetchStrategy === 'network first') {
-    event.respondWith(fetchNetworkFirst());
-  } else if (fetchStrategy === 'cache first') {
-    event.respondWith(
-      fetchFromCache()
-        .then(rememberToUpdateCache)
-        .catch(fetchNetworkFirst)
-    );
+  if (fetchStrategy.name === 'get index.html') {
+    respondCacheFirst(event, indexHtmlRequest);
+  } else if (fetchStrategy.name === 'prefer cache') {
+    respondCacheFirst(event, request);
+  } else if (fetchStrategy.name === 'prefer network') {
+    event.respondWith(fetchNetworkFirst(request)());
   }
 });
